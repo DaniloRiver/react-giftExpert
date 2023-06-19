6fccce9c933dd58871efa1c8b961440b97b5096c
Dani River <202101607@umh.edu.hn>
2023-06-19 17:04:29 -0600

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { AddCategory,GifGrid } from "./components";
+
+
+export const GifExpertApp = () => {
+  const [categorias, setCategorias] = useState(["One Punch"]);
+
+  const onAddCategory = (newCategory) => {
+    if (categorias.includes(newCategory)) return;
+
+    // console.log(newCategory);
+    setCategorias([newCategory, ...categorias]);
+  };
+
+//   console.log(categorias);
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory
+        // setCategories={setCategorias}
+        onNewCategory={(value) => onAddCategory(value)}
+      />
+
+      {categorias.map((category) => (
+        <GifGrid 
+            key={category} 
+            category={category} />
+      ))}
+    </>
+  );
+};
